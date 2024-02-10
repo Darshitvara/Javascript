38df732b7d6f9d683a08904bcc2e03e8f065d3a3
Darshit <darshitvara2004@gmail.com>
2024-02-10 19:19:37 +0530

    Moved files to the practice folder.

---
+++ Practice/index.js
@@ -0,0 +1,82 @@
+console.log("Hello Universe !");
+
+// way = 1 to take user input 
+// window.alert("Thank you!");
+
+
+// let name = window.prompt("Enter your name: ");
+// document.getElementById("p1").innerHTML = "Hello My name is " + name;
+
+// way = 2 to take user input 
+// let name;
+
+// document.getElementById("btn1").onclick = function(){
+//     name = document.getElementById("p2").value;
+//     document.getElementById("p1").innerHTML = "Hello " + name;
+// }
+
+
+// type conversions 
+
+// let age = 19;
+// let name = "Darshit";
+
+// console.log(typeof age);
+// console.log(typeof name);
+
+// age = String(age);
+// name = Number(name);
+// console.log(age,typeof age);
+// console.log(name ,typeof name);
+
+// Math functions
+
+// let x = window.prompt("Enter Number : ");
+// let y = window.prompt("Enter Number : ");
+
+// let roundx = Math.round(x)
+// let ceilx = Math.ceil(x);
+// let floorx = Math.floor(x);
+// let powx = Math.pow(x,2);
+// let sqrtx = Math.sqrt(x);
+// let absx = Math.abs(x);
+
+// let max = Math.max(x,y);
+// let min = Math.min(x,y);
+
+// let pi = Math.PI;
+// console.log(pi);
+
+
+
+// Program to find the hypotenus of right angle triangle 
+
+// document.getElementById("btn").onclick = function() {
+//     let val1 = Number(document.getElementById("input1").value);
+//     let val2 = Number(document.getElementById("input2").value);
+
+//     let answer = Math.sqrt(Math.pow(val1,2) + Math.pow(val2,2));
+
+//     document.getElementById("showAnswerHere").innerHTML = String(answer);
+
+// }
+
+
+// Up-down buttons
+
+let counter = 0;
+
+document.getElementById("btn1").onclick = function() {
+    counter++;
+    document.getElementById("number").innerHTML = String(counter);
+}
+
+document.getElementById("btn2").onclick = function() {
+    counter = 0;
+    document.getElementById("number").innerHTML = String(counter);
+}
+
+document.getElementById("btn3").onclick = function() {
+    counter--;
+    document.getElementById("number").innerHTML = String(counter);
+}
