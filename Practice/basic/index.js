95506ed4072ac182d5e1007a057a410d02d77c35
Darshit <darshitvara2004@gmail.com>
2024-06-07 11:53:23 +0530

    game included

--- Practice/basic/index.js
+++ Practice/basic/index.js
@@ -8,26 +8,18 @@ let name = window.prompt("Enter your name: ");
 document.getElementById("p1").innerHTML = "Hello My name is " + name;
 
 // way = 2 to take user input 
-// let name;
-
-// document.getElementById("btn1").onclick = function(){
-//     name = document.getElementById("p2").value;
-//     document.getElementById("p1").innerHTML = "Hello " + name;
-// }
 
 
+document.getElementById("btn1").onclick = function(){
+    name = document.getElementById("p2").value;
+    document.getElementById("p1").innerHTML = "Hello " + name;
+}
 
 
 
 
 
-// alert('hello darling')
 
-
-
-
-
-// Dice
 
 
 
---
+++ Practice/luck game/luck.js
@@ -0,0 +1,34 @@
+
+let number = Math.ceil(((Math.random() * 100)));
+// let number = 20;
+let guessedNo = 0;
+let counter = 0;
+document.getElementById("resetbtn").onclick = function(){
+    guessedNo = 0;
+    counter = 0;
+    number =  Math.ceil(((Math.random() * 100)));
+    document.getElementById("dicespace1").innerHTML = String(guessedNo);
+    document.getElementById("dicespace2").innerHTML = String("?");
+    document.getElementById("guessedNo").value = null
+}
+document.getElementById("btn").onclick = function(){
+    guessedNo = Number(document.getElementById("guessedNo").value) ;
+    document.getElementById("dicespace1").innerHTML = String(guessedNo);
+    counter++;
+    if(guessedNo == number){
+        document.getElementById("msg").innerHTML = "Congratulations..."+ " Guessed in " + String(counter) + " attempts.."
+        document.getElementById("dicespace2").innerHTML = String(guessedNo);
+    }
+    else if(guessedNo < number){
+        document.getElementById("msg").innerHTML = "Try again darling..go higher.."
+    }
+    else{
+        document.getElementById("msg").innerHTML = "Try again darling..go lower.."
+    }
+}
+
+
+
+
+
+
